b8d8848b9c053fb30a4373ad020bdc86e04d98cf
GrapesOne <m.d.f@ro.ru>
2021-02-25 19:12:28 +0500

    Update GameObjectInfo.cs

--- Assets/Scripts/GameObjectInfo.cs
+++ Assets/Scripts/GameObjectInfo.cs
@@ -4,16 +4,5 @@ using UnityEngine;
 
 public class GameObjectInfo : MonoBehaviour
 {
-    public GameObject WallLeft;
-    public GameObject WallBottom;
-    public GameObject Point;
-
-    private void Awake()
-    {
-        WallLeft = transform.GetChild(0).gameObject;
-        WallBottom = transform.GetChild(1).gameObject;
-        Point = transform.GetChild(2).gameObject;
-    }
-
     public List<string> Components = new List<string>();
 }
